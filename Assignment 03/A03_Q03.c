9475e74a7ba858a05300000677e86196e8b294c8
Abhijeet More <112226209+Abhijeet-Ambarushi-More@users.noreply.github.com>
2026-08-05 08:44:09 +0530

    Add files via upload

---
+++ Assignment 03/A03_Q03.c
@@ -0,0 +1,39 @@
+/* 
+WAP which accepts one number from user and print even factors of that number
+
+Input  : 36
+Output : 2   6   8   12
+
+*/
+
+#include<stdio.h>
+
+void DisplayEvenFactor(int iNo)
+{
+    int i = 0;
+
+   if (iNo < 0)
+    {
+        iNo=-iNo;
+    }
+
+    for(i = 1; i <= (iNo/2)  ; i++)
+    {
+        if((iNo % i) == 0 && ((i % 2) == 0))
+        {
+            printf("%d\t",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number\n");
+    scanf("%d",&iValue);
+
+    DisplayEvenFactor(iValue);
+
+    return 0;
+}
